7e1e16ed0ad0cabc6c127afa4cd85fc04769c9b6
hikerpig <hikerpigwinnie@gmail.com>
2021-02-09 13:27:15 +0800

    feat: update gatsby-config

--- _layouts/gatsby-config.js
+++ _layouts/gatsby-config.js
@@ -1,3 +1,5 @@
+const path = require('path')
+
 module.exports = {
   // pathPrefix: `/foam-template-gatsby-kb`, // a. If you are using github pages, this should be the name of your repo
   pathPrefix: `/`, // b. If you are using Netlify/Vercel, your can keep it this way
@@ -11,16 +13,27 @@ module.exports = {
     {
       resolve: `gatsby-theme-kb`,
       options: {
-        rootNote: "/readme",
+        rootNote: '/readme',
         contentPath: `${__dirname}/..`,
         ignore: [
-          "**/_layouts/**",
-          "**/.git/**",
-          "**/.github/**",
-          "**/.vscode/**",
-          "**/.cache/**",
+          '**/_layouts/**',
+          '**/.git/**',
+          '**/.github/**',
+          '**/.vscode/**',
+          '**/.cache/**',
         ],
       },
     },
+    {
+      // this plugin makes sure your static files will be served by gatsby,
+      // if you have multiple directories, copy this plugin section and specify other directory
+      // check https://github.com/csath/gatsby-plugin-copy-files-enhanced to find docs for this plugin
+      resolve: 'gatsby-plugin-copy-files-enhanced',
+      options: {
+        source: path.resolve(__dirname, `../assets`),
+        destination: '/assets',
+        purge: false,
+      },
+    },
   ],
-};
+}
