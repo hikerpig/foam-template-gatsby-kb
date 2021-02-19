264587a2514f97564d8d63d7b0d95c87ac4e2039
hikerpig <hikerpigwinnie@gmail.com>
2021-02-19 21:23:37 +0800

    test for deploy

--- _layouts/gatsby-config.js
+++ _layouts/gatsby-config.js
@@ -1,8 +1,10 @@
 const path = require('path')
 
+const PATH_PREFIX = process.env.PATH_PREFIX
+
 module.exports = {
-  // pathPrefix: `/foam-template-gatsby-kb`, // a. If you are using github pages, this should be the name of your repo
-  pathPrefix: `/`, // b. If you are using Netlify/Vercel, your can keep it this way
+  // pathPrefix: PATH_PREFIX || `/foam-template-gatsby-kb`, // a. If you are using github pages, this should be the name of your repo
+  pathPrefix: PATH_PREFIX || `/`, // b. If you are using Netlify/Vercel, your can keep it this way
   siteMetadata: {
     // some SEO configs using by gatsby-theme-kb
     title: `Foam`, // Replace it with your site's title
